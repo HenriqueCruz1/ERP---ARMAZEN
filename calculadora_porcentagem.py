510addc07e495048da01c18d0bb29e4f6f2de566
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-06-25 21:31:31 -0300

    inserindo calculadora de porcentagem

---
+++ calculadora_porcentagem.py
@@ -0,0 +1,16 @@
+def calculadora_desconto ():
+
+    valor = (float(input()))
+
+    desconto = int(input())
+
+    regra_de_desconto:float = (100 - desconto) / 100
+    calculo = valor * regra_de_desconto
+    return calculo
+
+print(calculadora_desconto())
+
+def calculadora_acrescimo():
+
+    return
+
