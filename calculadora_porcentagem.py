eba98e7c22f178d49d962b9b6a48e9455b43a105
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-06-25 23:17:57 -0300

    calc rules insert, button action

--- calculadora_porcentagem.py
+++ calculadora_porcentagem.py
@@ -1,16 +1,29 @@
 def calculadora_desconto ():
 
-    valor = (float(input()))
+    valor_desc = int(input('Insira o valor que recebera o desconto >;'))
 
-    desconto = int(input())
+    desconto = int(input('Quantos (%) de desconto deseja aplicar? '))
 
-    regra_de_desconto:float = (100 - desconto) / 100
-    calculo = valor * regra_de_desconto
+    regra_de_desconto = (100 - desconto) / 100
+    calculo = valor_desc * regra_de_desconto
+    
     return calculo
 
-print(calculadora_desconto())
 
 def calculadora_acrescimo():
 
-    return
+    valor_acre = int(input('Insira o valor que recebera o acréscimo >;'))
+
+    acrescimo = int(input('Quantos (%) de acréscimo deseja aplicar? '))
+
+    regra_de_acrescimo = (100 + acrescimo) / 100
+
+    calculo = valor_acre * regra_de_acrescimo
+
+    return calculo
+
+
+print(f'O valor atualizado com o desconto aplicado é: {calculadora_desconto()} R$')
+print(f'O valor atualizado com o acréscimo aplicado é: {calculadora_acrescimo()} R$')
+
 
