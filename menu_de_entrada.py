3a0bff88d5a4085d0d044c49d34b49b2d344665d
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-06-25 23:39:55 -0300

    try upgrade the logscreen

--- menu_de_entrada.py
+++ menu_de_entrada.py
@@ -1,17 +1,42 @@
-def login(email, senha):
+def verifica_email(email):
+
+    email = input()
 
     # Proteção de borda
 
-    if email == None or senha == None:
+    if email == None:
         return False
     
-    if email == '' or senha == '':
+    if email == '':
         return False
+    
+print(verifica_email)
+    
+def verifica_senha(senha):
+
+    senha = input()
+
+    if senha == None:
+        return False
+    
+    if senha == '':
+        return False
+
+print(verifica_senha)
+
+
   #   Proteção de négocio
+
 
 #leia a string, verifique a existencia de um caractere especifico. 
 
+print(15*'-', 'EMAIL//LOGIN* ')
 
+verifica_email()
+
+print(15*'-', 'SENHA//LOGIN* ')
+
+verifica_senha()
     
 
 
